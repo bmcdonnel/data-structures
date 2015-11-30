e5c8f709f462d76cf81bebfb09fa75620c178d96
Bryan McDonnel <bryan.mcdonnel@gmail.com>
2015-11-29 19:50:08 -0600

    cleaned up index child/parent movement

--- algorithms/bst_node_path.cpp
+++ algorithms/bst_node_path.cpp
@@ -3,20 +3,6 @@
 
 #include "utils/binary_search_tree.h"
 
-uint32_t log_base_2(const uint32_t value)
-{
-  uint32_t temp = value;
-  uint32_t division_count = 0;
-
-  while (temp > 0)
-  {
-    temp = temp >> 1;
-    division_count++;
-  }
-
-  return division_count - 1;
-}
-
 int main(int argc, char** argv)
 {
   if (argc < 2)
--- utils/binary_search_tree.h
+++ utils/binary_search_tree.h
@@ -1,13 +1,29 @@
 #include <sstream>
 
+namespace utils {
+  uint32_t log_base_2(const uint32_t value)
+  {
+    uint32_t temp = value;
+    uint32_t division_count = 0;
+
+    while (temp > 0)
+    {
+      temp = temp >> 1;
+      division_count++;
+    }
+
+    return division_count - 1;
+  }
+}
+
 template <typename T>
 class BinarySearchTree
 {
 public:
   BinarySearchTree(const uint32_t initial_num_elements) :
-    _array_size(initial_num_elements),
     _item_count(0)
   {
+    _array_size = (2 << utils::log_base_2(initial_num_elements)) + 1;
     _array = new Node[_array_size];
   }
 
@@ -23,24 +39,27 @@ public:
 
   void Insert(const T& value)
   {
-    uint32_t i = 0;
-    while (i < _array_size)
+    uint32_t index = 0;
+
+    while (index < _array_size)
     {
-      if (_array[i].is_empty)
+      if (_array[index].is_empty)
       {
-        _array[i].value = value;
-        _array[i].is_empty = false;
+        _array[index].value = value;
+        _array[index].is_empty = false;
 
         _item_count++;
+
+        Rebalance(ParentIndex(index));
         break;
       }
-      else if (value <= _array[i].value)
+      else if (value <= _array[index].value)
       {
-        i = (2 * i + 1);
+        index = LeftChildIndex(index);
       }
-      else if (value > _array[i].value)
+      else if (value > _array[index].value)
       {
-        i = (2 * i + 2);
+        index = RightChildIndex(index);
       }
     }
   }
@@ -88,17 +107,30 @@ private:
 
   void Rebalance(const uint32_t index)
   {
+    _array[index].balance = Height(RightChildIndex(index)) - Height(LeftChildIndex(index));
+
+    if (_array[index].balance == -2)
+    {
+    }
+    else if (_array[index].balance == 2)
+    {
+    }
+
+    if (index != 0)
+    {
+      Rebalance(ParentIndex(index));
+    }
   }
 
   uint32_t Height(const uint32_t index) const
   {
-    if (_array[index].is_empty)
+    if (index >= _item_count || _array[index].is_empty)
     {
       return 0;
     }
 
-    uint32_t leftHeight = Height(index * 2 + 1);
-    uint32_t rightHeight = Height(index * 2 + 2);
+    uint32_t leftHeight = Height(LeftChildIndex(index));
+    uint32_t rightHeight = Height(RightChildIndex(index));
 
     if (leftHeight > rightHeight)
     {
@@ -108,5 +140,20 @@ private:
     {
       return rightHeight + 1;
     }
+  }
+
+  static uint32_t LeftChildIndex(const uint32_t index)
+  {
+    return index * 2 + 1;
+  }
+
+  static uint32_t RightChildIndex(const uint32_t index)
+  {
+    return index * 2 + 2;
+  }
+
+  static uint32_t ParentIndex(const uint32_t index)
+  {
+    return (index - 1) / 2;
   }
 };
