4e778d565c444f2bdb9a853a8c9cd1192042af99
Bryan McDonnel <bryan.mcdonnel@gmail.com>
2016-01-14 20:59:30 -0600

    started a lowest common ancestor exercise

---
+++ exercises/lowest_common_ancestor.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <set>
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+TreeNode* lowest_common_ancestor(TreeNode* root, TreeNode* node_a, TreeNode* node_b)
+{
+  return nullptr;
+}
+
+int main(int argc, char** argv)
+{
+  return 0;
+}
