b2d44a78611c45583e19e195e7dcf5ea5d66142f
Bryan McDonnel <bryan.mcdonnel@gmail.com>
2016-01-16 11:51:37 -0600

    finished lowest common ancestor

--- exercises/lowest_common_ancestor.cpp
+++ exercises/lowest_common_ancestor.cpp
@@ -10,7 +10,21 @@ struct TreeNode {
 
 TreeNode* lowest_common_ancestor(TreeNode* root, TreeNode* node_a, TreeNode* node_b)
 {
-  return nullptr;
+  if (node_a->val < root->val &&
+      node_b->val < root->val)
+  {
+    // if both A and B are to the left of root; go to the left
+    return lowest_common_ancestor(root->left, node_a, node_b);
+  }
+  else if (node_a->val > root->val &&
+           node_b->val > root->val)
+  {
+    // if both A and B are to the right of root; go to the right
+    return lowest_common_ancestor(root->right, node_a, node_b);
+  }
+
+  // A and B are on opposite sites of root; this is the lowest common ancestor
+  return root;
 }
 
 int main(int argc, char** argv)
