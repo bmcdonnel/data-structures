b210c1d9216198756784268336d663d2c8d46499
Bryan McDonnel <bryan.mcdonnel@gmail.com>
2016-01-19 20:11:04 -0600

    added a list intersection exercise

---
+++ exercises/list_intersection.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+ListNode* find_intersection_node(ListNode* node_a, ListNode* node_b)
+{
+  ListNode* i = node_a;
+  ListNode* j = node_b;
+
+  while(i != nullptr && j != nullptr && i != j)
+  {
+    i = i->next;
+    j = j->next;
+
+    if (i == j)
+    {
+      return i;
+    }
+
+    // if either pointer reaches the end,
+    // reset it to the front of the opposite list
+    // once both pointers reset to their opposite lists,
+    // both will be the same distance away from any 
+    // potential intersection
+    if (i == nullptr) { i = node_b; }
+    if (j == nullptr) { j = node_a; }
+  }
+
+  return i;
+}
