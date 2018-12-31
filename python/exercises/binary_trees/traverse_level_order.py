ef0b570ff9f151994b438c705e8d739e5f1f0f10
Bryan McDonnel <bryan.mcdonnel@gmail.com>
2018-12-31 15:30:40 -0600

    add traverse_level_order

---
+++ python/exercises/binary_trees/traverse_level_order.py
@@ -0,0 +1,28 @@
+import sys
+from queue import Queue
+from exercises.binary_trees.node import Node
+
+def traverse_level_order(tree):
+    if tree == None:
+        return
+
+    q = Queue()
+
+    q.put(tree)
+
+    while q.qsize():
+        node = q.get()
+
+        print(node)
+
+        if node.left:
+            q.put(node.left)
+
+        if node.right:
+            q.put(node.right)
+
+if __name__ ==  "__main__":
+    values = input("Enter tree (implicit ordering; use 'x' for empty node): ").split(" ")
+    tree = Node.build_from_implicitly_ordered_list(values)
+
+    traverse_level_order(tree)
