df0909b20c922228a2b9645a715bc4ca66ba9116
Bryan McDonnel <bryan.mcdonnel@gmail.com>
2019-01-11 21:19:36 -0600

    add graph class

---
+++ python/exercises/common/graph.py
@@ -0,0 +1,38 @@
+from exercises.common.vertex import Vertex
+
+class Graph:
+    def __init__(self, is_bidirectional=True):
+        self._vertices = {}
+        self._is_bidirectional = True
+
+    def is_bidirectional(self):
+        return self._is_bidirectional
+
+    def add_vertex(self, key):
+        vertex = Vertex(key)
+        self._vertices[key] = vertex
+
+        return vertex
+
+    def add_edge(self, from_key, to_key, weight):
+        if from_key not in self._vertices:
+            self.add_vertex(from_key)
+
+        from_vertex = self._vertices[from_key]
+
+        if to_key not in self._vertices:
+            self.add_vertex(to_key)
+
+        to_vertex = self._vertices[to_key]
+
+        from_vertex.add_neighbor(to_vertex, weight)
+
+        if self.is_bidirectional():
+            to_vertex.add_neighbor(from_vertex, weight)
+
+    @staticmethod
+    def build_from_list(values):
+        return Graph()
+
+    def __str__(self):
+        return "\n".join([str(v) for v in self._vertices.values()])
