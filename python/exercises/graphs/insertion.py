3a46bd2c1ec6301b938a1a02fbed53a2f8cf0ba3
Bryan McDonnel <bryan.mcdonnel@gmail.com>
2019-01-11 21:36:25 -0600

    add support for entering weights

--- python/exercises/graphs/insertion.py
+++ python/exercises/graphs/insertion.py
@@ -5,12 +5,16 @@ if __name__ ==  "__main__":
     try:
         graph = Graph()
         while True:
-            vertices = input("Enter vertices to create edges along a path: ").strip().split(" ")
-            for i in range(len(vertices) - 1):
+            vertices = input("Enter vertices and weights to create edges (eg - a 1 b 2 c): ").strip().split(" ")
+            i = 0
+            while i < (len(vertices) - 2):
                 v1 = vertices[i]
-                v2 = vertices[i + 1]
+                weight = int(vertices[i + 1])
+                v2 = vertices[i + 2]
 
-                graph.add_edge(v1, v2, 1)
+                graph.add_edge(v1, v2, weight)
+
+                i += 2
 
             print(graph)
     except EOFError:
