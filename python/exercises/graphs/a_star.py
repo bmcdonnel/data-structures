19e13a0dcd081e1ec338f496e09c77fa3659e1fa
Bryan McDonnel <bryan.mcdonnel@gmail.com>
2019-01-12 19:32:04 -0600

    finish A* logic

--- python/exercises/graphs/a_star.py
+++ python/exercises/graphs/a_star.py
@@ -14,11 +14,40 @@ def a_star(maze, start, end):
         if current_node == end:
             break
 
+        for neighbor in current_node.neighbors():
+            if (neighbor in closed_nodes or
+                neighbor in open_nodes or
+                neighbor.blocked):
+                continue
+
+            x_diff = end.x() - neighbor.x()
+            y_diff = end.y() - neighbor.y()
+
+            # TODO maybe precompute these?
+            neighbor.g = current_node.g + 1
+            neighbor.h = (x_diff * x_diff) + (y_diff * y_diff)
+            neighbor.f = neighbor.g + neighbor.h
+            neighbor.parent = current_node
+
+            open_nodes.append(neighbor)
+
+    if current_node != end:
+        return None
+
+    path = []
+    while current_node != start:
+        path.append(current_node)
+        current_node = current_node.parent
+
+    path.append(start)
+
+    return list(reversed(path))
+
 def pop_minimum_f(nodes):
     node = nodes[0]
     index = 0
 
-    for i, x in enumerate(thing[1:]):
+    for i, x in enumerate(nodes[1:]):
         if x.f < node.f:
             node = x
             index = i + 1
@@ -30,6 +59,11 @@ def pop_minimum_f(nodes):
 if __name__ ==  "__main__":
     maze, start, end = maze_input()
 
-    [print(line) for line in maze]
-
     path = a_star(maze, start, end)
+
+    # mark the chosen path
+    for node in path[1:-1]:
+        node.value = "o"
+
+    for line in maze:
+        print(" ".join([x.value for x in line]))
