f5b3453cecc082cb6d4d44b2d464dd253fd8e907
Bryan McDonnel <bryan.mcdonnel@gmail.com>
2019-02-03 22:05:42 -0600

    move concurrency problems to their own module

--- python/exercises/concurrency/smokers_problem.py
+++ python/exercises/concurrency/smokers_problem.py
@@ -3,15 +3,6 @@ import time
 
 from multiprocessing import Process, Lock, Semaphore, Event, Value
 
-class StoppableProcess(Process):
-    def __init__(self, stop_event):
-        super().__init__()
-
-        self.stop_event = stop_event
-
-    def _should_run(self):
-        return not self.stop_event.is_set()
-
 class Smoker(StoppableProcess):
     def __init__(
             self,
