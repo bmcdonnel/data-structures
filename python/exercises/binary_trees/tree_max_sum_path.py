bebd1eb43cd8c047665a06994b939156bbe8858d
Bryan McDonnel <bryan.mcdonnel@gmail.com>
2019-02-23 16:45:36 -0600

    improve max sum path

--- python/exercises/binary_trees/tree_max_sum_path.py
+++ python/exercises/binary_trees/tree_max_sum_path.py
@@ -1,34 +1,52 @@
 import sys
 from exercises.common.node import Node
 
-def max_sum_path(node):
-    sum_dict = dict()
+def max_root_leaf_sum(node, path=list()):
+    if node is None:
+        return 0, path
 
-    __sum_paths(node, [], 0, sum_dict)
+    left_sum, left_path = max_root_leaf_sum(node.left, path)
+    right_sum, right_path = max_root_leaf_sum(node.right, path)
 
-    result = 0
-    node_path = []
+    if left_sum >= right_sum:
+        max_sum = left_sum
+        max_path = left_path
+    else:
+        max_sum = right_sum
+        max_path = right_path
 
-    for key, value in sum_dict.items():
-        if value[1] > result:
-            node_path = value[0]
-            result = value[1]
+    return (max_sum + node.value, [node] + max_path)
 
-    return node_path, result
+def max_any_path_sum(root):
+    def _max_any_path_sum(node):
+        if node is None:
+            return 0
 
-def __sum_paths(node, path, current_sum, sum_dict):
-    if node == None:
-        sum_dict[path[-1].value] = (path, current_sum)
-        return
+        left_sum = _max_any_path_sum(node.left)
+        right_sum = _max_any_path_sum(node.right)
 
-    __sum_paths(node.left, path + [node], current_sum + node.value, sum_dict)
-    __sum_paths(node.right, path + [node], current_sum + node.value, sum_dict)
+        value = max(
+            left_sum + node.val + right_sum,
+            left_sum + node.val,
+            right_sum + node.val,
+            node.val,
+        )
+
+        max_sum[0] = max(value, max_sum[0])
+
+        return max(left_sum + node.val, right_sum + node.val, node.val)
+
+	max_sum = [-2**31]
+	_max_any_path_sum(root)
+
+	return max_sum[0]
+
 
 if __name__ ==  "__main__":
     values = input("Enter tree (implicit ordering; use 'x' for empty node): ").strip().split(" ")
     tree = Node.build_from_implicitly_ordered_list(values)
 
-    node_path, max_sum = max_sum_path(tree)
+    max_sum, max_path = max_root_leaf_sum(tree)
 
     print("Max sum path is {} with path:".format(max_sum))
-    print([x.value for x in node_path])
+    print([x.value for x in max_path])
